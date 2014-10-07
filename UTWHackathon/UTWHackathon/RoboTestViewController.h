4aede139951c87c8aad2547c556f51653c854102
Michael Smith <MSmith@ibfx.com>
2014-10-07 13:09:44 -0600

    Added view controller

---
+++ UTWHackathon/UTWHackathon/RoboTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoboTestViewController.h
+//  UTWHackathon
+//
+//  Created by Michael Smith on 10/7/14.
+//  Copyright (c) 2014 TradeStation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoboTestViewController : UIViewController
+
+@end
---
+++ UTWHackathon/UTWHackathon/RoboTestViewController.m
@@ -0,0 +1,45 @@
+//
+//  RoboTestViewController.m
+//  UTWHackathon
+//
+//  Created by Michael Smith on 10/7/14.
+//  Copyright (c) 2014 TradeStation. All rights reserved.
+//
+
+#import "RoboTestViewController.h"
+
+@interface RoboTestViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
+@property (weak, nonatomic) IBOutlet UIButton *backwardButton;
+@property (weak, nonatomic) IBOutlet UIButton *stopButton;
+@property (weak, nonatomic) IBOutlet UIButton *redColorButton;
+@property (weak, nonatomic) IBOutlet UIButton *blueColorButton;
+@property (weak, nonatomic) IBOutlet UILabel *locationLabel;
+
+@end
+
+@implementation RoboTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+- (IBAction)forwardButtonPressed:(id)sender {
+	
+}
+- (IBAction)stopButtonPressed:(id)sender {
+	
+}
+- (IBAction)backwardButtonPressed:(id)sender {
+	
+}
+- (IBAction)redColorButtonPressed:(id)sender {
+	
+}
+- (IBAction)blueColorButtonPressed:(id)sender {
+	
+}
+
+@end
